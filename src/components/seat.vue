3a7cd97c35cb827a4192f103ae6a41652fac6197
Xingxin Zeng <star_yes@qq.com>
2018-05-12 10:57:01 +0800

    Add playground and seat components

---
+++ src/components/seat.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="seat" @tap="handleViewTap">
+    <p class="seat-number">{{ num + 1 }}</p>
+    <img class="seat-avatar" :src="avatarUrl"/>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    avatarUrl: {
+      type: String
+    },
+    num: {
+      type: Number
+    }
+  },
+
+  methods: {
+    handleViewTap () {
+      this.$emit('seatDown', this.num)
+    }
+  }
+}
+</script>
+
+<style>
+.seat {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 5px;
+}
+
+.seat-number {
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-color: #444;
+  color: #eee;
+  border-radius: 4rpx;
+  width: 30rpx;
+  height: 30rpx;
+  font-size: 20rpx;
+  line-height: 30rpx;
+  text-align: center;
+}
+
+.seat-avatar {
+  width: 120rpx;
+  height: 120rpx;
+  border-radius: 15rpx;
+}
+</style>
---
+++ src/pages/playground/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="playground-warp">
+    <p>playground</p>
+    <seat :num="1"></seat>
+    <a href="/pages/index/main" class="home">去往首页</a>
+  </div>
+</template>
+
+<script>
+
+import seat from '@/components/seat'
+
+export default {
+  components: {
+    seat
+  }
+}
+
+</script>
+<style>
+.playground-warp {
+  text-align: center;
+  margin-top: 100px;
+}
+.home {
+  display: inline-block;
+  margin: 100px auto;
+  padding: 5px 10px;
+  color: blue;
+  border: 1px solid blue;
+}
+
+</style>
